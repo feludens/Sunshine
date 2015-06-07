8cea1095d3b308c5682cfa0f6025d999757ce258
Felipe S. Pereira <fe.ludens@gmail.com>
2015-06-07 16:34:23 -0400

    Web Services, AsynTask, and start of refresh button

--- app/src/main/java/com/spadatech/sunshine/MainActivity.java
+++ app/src/main/java/com/spadatech/sunshine/MainActivity.java
@@ -1,12 +1,14 @@
 package com.spadatech.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
